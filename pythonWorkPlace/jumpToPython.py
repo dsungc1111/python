d591d941aac79a015e383f27b1ef263c1e214b4c
dsungc1111 <eotjd9808@naver.com>
2023-01-17 15:16:50 +0900

    while문

---
+++ pythonWorkPlace/jumpToPython.py
@@ -0,0 +1,14 @@
+coffee = 10
+
+while True:
+    money = int(input("돈을 넣어주세요 : "))
+    if money == 300:
+        print("Here is coffee~")
+        coffee = coffee - 1
+    elif money > 300:
+        print("coffee and change %dwon is here" % (money-300))
+        coffee = coffee - 1
+    else:
+        print("money isn't enough, remain coffee : %d" % coffee)
+    if coffee == 0:
+        print("stop selling")
